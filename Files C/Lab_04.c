21f1dd6fc71b655febb534d9a860d65c29737854
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-01-21 19:05:23 +0700

    Add files via upload

---
+++ Files C/Lab_04.c
@@ -0,0 +1,10 @@
+/* File Name : Lab_04.c */
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	printf("\"Kasetsart University\"\n");
+	printf("\n\t\'Kasetsart University\'\n");
+	printf("\n\t\t** Faculty of Liberal Arts and Sciences **");
+	getch();
+}
---
+++ Files C/Lab_05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	char alphabet;
+	printf("Please inpuit a Alphabet : ");
+	scanf("%c",&alphabet);
+	printf("ASCII CODE : %d",alphabet);
+	getch();
+}
