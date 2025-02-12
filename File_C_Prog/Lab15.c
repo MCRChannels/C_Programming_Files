4c2941b894a6a72f5493c0d113a5274339b56416
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-02-12 15:45:52 +0700

    Update Lab15.c

--- File_C_Prog/Lab15.c
+++ File_C_Prog/Lab15.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
+#include <conio.h>
 main()
 {
 	int min_number , max_number , is_prime;
 	int i,j;
+	int counting = 0;
 	printf("This Program find prime numbers\n");
-	printf("Please enter min_number : ");
-	scanf("%d",&min_number);
-	printf("Please enter max_number : ");
-	scanf("%d",&max_number);
 
-	printf("\n*** Your prime numbers is ***\n\n");
+	do {
+		printf("Please enter min_number : ");
+		scanf("%d",&min_number);
+		printf("Please enter max_number : ");
+		scanf("%d",&max_number);
+
+		if ( min_number >= max_number ) {
+			printf("\n** min number must lower than max number kub **");
+			printf("\nPlease input value again\n");
+			printf("\n");
+		}
+
+
+	} while (min_number >= max_number );
+
+	printf("\n*** Your prime numbers is ***\n");
 
 	for (i = min_number ; i <= max_number ; i++) {
 		is_prime = 1;
 
-		for ( j = 2 ; j < i ; j++ ) {
-			if(i % j == 0 ){
-				is_prime = 0;
-				break;
+		if ( i < 2 ) {
+			is_prime = 0;
+		} else {
+			for ( j = 2 ; j * j <= i ; j++ ) {
+				if(i % j == 0){
+					is_prime = 0;
+					printf("\nThis [%d] is not Prime Number in : %d",i,j);
+					break;
+				}
 			}
 		}
 
-		if(is_prime == 1){
-			printf("%d ",i);
+		if(is_prime){
+			printf("\tThis [%d] is **Prime Number**",i);
+			counting++;
 		}
 	}
-	getch();
 
+	printf("\n\nAll Prime number is : %d",counting);
+	getch();
 }
