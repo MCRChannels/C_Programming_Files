210956d275ea70e962002bacc7a6d18618b9069b
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-02-12 16:22:34 +0700

    Update Lab24.c

--- File_C_Prog/Lab24.c
+++ File_C_Prog/Lab24.c
@@ -1,36 +1,63 @@
 #include <stdio.h>
-#define Max_Size 5
+#define Max_Size 100
 int main(){
 	int A[Max_Size][Max_Size], B[Max_Size][Max_Size], C[Max_Size][Max_Size];
+	int size_i,size_j;
 	int i,j;
 	int sum = 0;
 	float average = 0;
 
-	for ( i = 0 ; i < Max_Size ; i++ ){
-		for ( j = 0 ; j < Max_Size ; j++ ){
+	printf("Please enter Size of Array2D [i] : ");
+	while (scanf("%d",&size_i) != 1){
+		printf("\nDon't input Alphabet");
+		printf("\nPlease input again!");
+		printf("\nPlease enter Size of Array2D [i] : ");
+		while(getchar() != '\n');
+	}
+
+	printf("Please enter Size of Array2D [j] : ");
+	while (scanf("%d",&size_j) != 1){
+		printf("\nDon't input Alphabet");
+		printf("\nPlease input again!");
+		printf("\nPlease enter Size of Array2D [j] : ");
+		while(getchar() != '\n');
+	}
+
+	for ( i = 0 ; i < size_i ; i++ ){
+		for ( j = 0 ; j < size_j ; j++ ){
 			printf("Please enter Value in A[%d][%d] : ",i,j);
-			scanf("%d",&A[i][j]);
+			while(scanf("%d",&A[i][j]) != 1){
+				printf("\nDon't input Alphabet!");
+				printf("\nPlease input again");
+				printf("\nPlease enter Value in A[%d][%d] : ",i,j);
+				while(getchar() != '\n');
+			}
 		}
 	}
 
-	for ( i = 0 ; i < Max_Size ; i++ ){
-		for ( j = 0 ; j < Max_Size ; j++ ){
+	for ( i = 0 ; i < size_i ; i++ ){
+		for ( j = 0 ; j < size_j ; j++ ){
 			printf("Please enter Value in B[%d][%d] : ",i,j);
-			scanf("%d",&B[i][j]);
+			while(scanf("%d",&B[i][j]) != 1){
+				printf("\nDon't input Alphabet!");
+				printf("\nPlease input again");
+				printf("\nPlease enter Value in B[%d][%d] : ",i,j);
+				while(getchar() != '\n');
+			}
 		}
 	}
 
 
 	printf("\n=========================\n");
-	for ( i = 0 ; i < Max_Size ; i++ ){
-		for ( j = 0 ; j < Max_Size ; j++ ){
+	for ( i = 0 ; i < size_i ; i++ ){
+		for ( j = 0 ; j < size_j ; j++ ){
 			C[i][j] = A[i][j] * B[i][j];
 			sum += C[i][j];
 			printf("C[%d][%d] is : %d\n",i,j,C[i][j]);
 		}
 	}
 
-	average = sum / (Max_Size * Max_Size);
+	average = sum / (size_i * size_j);
 
 	printf("\n\n**Average Array C is : %.2f\n",average);
 
