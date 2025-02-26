3d0c6758d645dc377fa54dc19c295c292e1f64c9
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-02-26 16:22:42 +0700

    Update Lab20.c

--- File_C_Prog/Lab20.c
+++ File_C_Prog/Lab20.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
 void func1();
 void func2();
-int global = 40;
 
+int global = 40; // global var
 
 main()
 {
-	int localmain = 20;
+	int localmain = 20; // local var
 	printf("Show globalVariable : %d\n",global);
 	printf("Show localVariable : %d\n",localmain);
 
 	func1();
 	func2();
-
-	//printf("Show localVar from func1 in main : %d\n",local);
 }
 
 void func1()
 {
-	int localfunc1 = 10;
+	int localfunc = 10; //local var
 	printf("\nThis is in function 1 kub!");
 	printf("\nShow globalVariable : %d",global);
-	printf("\nShow localVariable : %d\n",localfunc1);
+	printf("\nShow localVariable : %d\n",localfunc);
 }
 
 void func2()
 {
-	int localfunc2 = 46;
+	int localfunc = 46; //local var
 	printf("\nThis is in function 2 kub!");
 	printf("\nShow globalVariable : %d",global);
-	printf("\nShow localVariable : %d\n",localfunc2);
+	printf("\nShow localVariable : %d\n",localfunc);
 
-	//printf("\n\nShow localVaruabke from func1 in func2 : %d\n",localfunc1);
-
+	//printf("\n\nShow localmain from main in func2 : %d\n",localmain);
 }
