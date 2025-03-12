0d7b88616df431e292da9ee20f7a561e978ffd0a
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-03-13 02:29:32 +0700

    Update Lab15.c

--- File_C_Prog/Lab15.c
+++ File_C_Prog/Lab15.c
@@ -5,46 +5,51 @@ main()
 	int min_number , max_number , is_prime;
 	int i,j;
 	int counting = 0;
-	printf("This Program find prime numbers\n");
+	char end;
 
-	do {
-		printf("Please enter min_number : ");
-		scanf("%d",&min_number);
-		printf("Please enter max_number : ");
-		scanf("%d",&max_number);
+	do{
+		printf("This Program find prime numbers\n");
 
-		if ( min_number >= max_number ) {
-			printf("\n** min number must lower than max number kub **");
-			printf("\nPlease input value again\n");
-			printf("\n");
-		}
+		do {
+			printf("Please enter min_number : ");
+			scanf("%d",&min_number);
+			printf("Please enter max_number : ");
+			scanf("%d",&max_number);
+
+			if ( min_number >= max_number ) {
+				printf("\n** min number must lower than max number kub **");
+				printf("\nPlease input value again\n");
+				printf("\n");
+			}
 
 
-	} while (min_number >= max_number );
+		} while (min_number >= max_number );
 
-	printf("\n*** Your prime numbers is ***\n");
+		printf("\n*** Your prime numbers is ***\n");
 
-	for (i = min_number ; i <= max_number ; i++) {
-		is_prime = 1;
+		for (i = min_number ; i <= max_number ; i++) {
+			is_prime = 1;
 
-		if ( i < 2 ) {
-			is_prime = 0;
-		} else {
-			for ( j = 2 ; j * j <= i ; j++ ) {
-				if(i % j == 0){
-					is_prime = 0;
-					printf("\nThis [%d] is not Prime Number in : %d",i,j);
-					break;
+			if ( i < 2 ) {
+				is_prime = 0;
+			} else {
+				for ( j = 2 ; j * j <= i ; j++ ) {
+					if(i % j == 0){
+						is_prime = 0;
+						printf("\nThis [%d] is not Prime Number in : %d",i,j);
+						break;
+					}
 				}
+			}
+
+			if(is_prime){
+				printf("\tThis [%d] is **Prime Number**",i);
+				counting++;
 			}
 		}
 
-		if(is_prime){
-			printf("\tThis [%d] is **Prime Number**",i);
-			counting++;
-		}
-	}
-
-	printf("\n\nAll Prime number is : %d",counting);
-	getch();
+		printf("\n\nAll Prime number is : %d",counting);
+		printf("do you want to continue? (Y/N) : ");
+		scanf(" %c",&end);
+	}while (end != 'n' && end != 'N');
 }
