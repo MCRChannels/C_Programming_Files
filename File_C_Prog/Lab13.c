76ca585d9d0890fb9c2510ced880799d24574980
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-03-13 02:28:52 +0700

    Update Lab13.c

--- File_C_Prog/Lab13.c
+++ File_C_Prog/Lab13.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 main ()
 {
-	char c1,c2,c3,c4,c5,temp;
+	char c1,c2,c3,c4,c5,temp,end;
 	int sorted = 0;
 
-	printf("Please enter 5 Charactor to sorted : ");
-	scanf("%c %c %c %c %c",&c1,&c2,&c3,&c4,&c5);
 
-	while(sorted == 0) {
-		sorted = 1;
-		while(c1 > c2) {
-			temp = c1 ;
-			c1 = c2;
-			c2 = temp;
-			sorted = 0;
-		}
-		while(c2 > c3) {
-			temp = c2;
-			c2 = c3;
-			c3 = temp;
-			sorted = 0;
-		}
-		while(c3 > c4) {
-			temp = c3;
-			c3 = c4;
-			c4 = temp;
-			sorted = 0;
-		}
-		while(c4 > c5){
-			temp = c4;
-			c4 = c5;
-			c5 = temp;
-			sorted = 0;
+	do{
+		printf("Please enter 5 Charactor to sorted : ");
+		scanf("%c %c %c %c %c",&c1,&c2,&c3,&c4,&c5);
+
+		while(sorted == 0) {
+			sorted = 1;
+			while(c1 > c2) {
+				temp = c1 ;
+				c1 = c2;
+				c2 = temp;
+				sorted = 0;
+			}
+			while(c2 > c3) {
+				temp = c2;
+				c2 = c3;
+				c3 = temp;
+				sorted = 0;
+			}
+			while(c3 > c4) {
+				temp = c3;
+				c3 = c4;
+				c4 = temp;
+				sorted = 0;
+			}
+			while(c4 > c5){
+				temp = c4;
+				c4 = c5;
+				c5 = temp;
+				sorted = 0;
+			}
+
 		}
 
-	}
-
-	printf("\n\n==|Your Charactor is|== : %c %c %c %c %c",c1,c2,c3,c4,c5);
+		printf("\n\nsorted : %c %c %c %c %c",c1,c2,c3,c4,c5);
+		printf("do you want to continue? (Y/N) : ");
+		scanf(" %c",&end);
+	}while (end != 'N' && end != 'n');
 }
