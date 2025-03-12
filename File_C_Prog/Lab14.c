958d44fe1a644143bf64d2c9e20a5aba6656208e
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-03-13 02:29:02 +0700

    Update Lab14.c

--- File_C_Prog/Lab14.c
+++ File_C_Prog/Lab14.c
@@ -2,48 +2,52 @@
 main()
 {
 	int one_number, two_number , max_number , amount;
-
-	printf("Please enter one_number : ");
-	while(scanf("%d",&one_number) != 1){
-		printf("Dont input Alphabet\n");
-		printf("Please enter one_number : ");
-		while(getchar() != '\n');   // clear buffer
-	}
-
-	printf("Please enter two_number : ");
-	while (scanf("%d",&two_number) != 1){
-		printf("Dont input Alphabet\n");
-		printf("Please enter one_number : ");
-		while(getchar() != '\n');
-	}
-
-	printf("Please set the maximum value number : ");
-	scanf("%d",&max_number);
-
-	amount = one_number + two_number;
+	char end;
 
 	do {
-		if (amount >= max_number) {
-			printf("Value is more than your max_number. Try again!\n");
-
+		printf("Please enter one_number : ");
+		while(scanf("%d",&one_number) != 1){
+			printf("Dont input Alphabet\n");
 			printf("Please enter one_number : ");
-			while(scanf("%d",&one_number) != 1){
-				printf("Don't input Alphabet\n");
-				printf("Please enter one_number : ");
-				while(getchar() != '\n');
-			}
-
-			printf("Please enter two_number : ");
-			while(scanf("%d", &two_number) != 1){
-				printf("Don't input Alphabet\n");
-				printf("Please enter two_number : ");
-				while(getchar() != '\n');
-			}
-
-			amount = one_number + two_number;
+			while(getchar() != '\n');
 		}
-	} while (amount >= max_number);
 
-	printf("\n\n\nMax is \"%d\" and sum of the number is %d",max_number,amount);
-	getch();
+		printf("Please enter two_number : ");
+		while (scanf("%d",&two_number) != 1){
+			printf("Dont input Alphabet\n");
+			printf("Please enter one_number : ");
+			while(getchar() != '\n');
+		}
+
+		printf("Please set the maximum value number : ");
+		scanf("%d",&max_number);
+
+		amount = one_number + two_number;
+
+		do {
+			if (amount >= max_number) {
+				printf("Value is more than your max_number. Try again!\n");
+
+				printf("Please enter one_number : ");
+				while(scanf("%d",&one_number) != 1){
+					printf("Don't input Alphabet\n");
+					printf("Please enter one_number : ");
+					while(getchar() != '\n');
+				}
+
+				printf("Please enter two_number : ");
+				while(scanf("%d", &two_number) != 1){
+					printf("Don't input Alphabet\n");
+					printf("Please enter two_number : ");
+					while(getchar() != '\n');
+				}
+
+				amount = one_number + two_number;
+			}
+		} while (amount >= max_number);
+
+		printf("\n\n\nMax is \"%d\" and sum of the number is %d\n",max_number,amount);
+		printf("do you want to continue? (Y/N) : ");
+		scanf(" %c",&end);
+	} while (end != 'n' && end != 'N');
 }
