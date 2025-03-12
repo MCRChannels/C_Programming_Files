ae89828880a3053804427d48f69dd95c4de04be9
NotThisDayZ_ <47767969+MCRChannels@users.noreply.github.com>
2025-03-13 02:28:41 +0700

    Update Lab12.c

--- File_C_Prog/Lab12.c
+++ File_C_Prog/Lab12.c
@@ -3,28 +3,35 @@ main()
 {
 	int min_number , max_number , is_prime;
 	int i,j;
-	printf("This Program find prime numbers\n");
-	printf("Please enter min_number : ");
-	scanf("%d",&min_number);
-	printf("Please enter max_number : ");
-	scanf("%d",&max_number);
+	char end;
 
-	printf("\n*** Your prime numbers is ***\n\n");
+	do{
+		printf("This Program find prime numbers\n");
+		printf("Please enter min_number : ");
+		scanf("%d",&min_number);
+		printf("Please enter max_number : ");
+		scanf("%d",&max_number);
 
-	for (i = min_number ; i <= max_number ; i++) {
-		is_prime = 1;
+		printf("\n*** Your prime numbers is ***\n\n");
 
-		j = 2;
-		while (j < i ){
-			is_prime *= (i%j != 0);
-			j++;
+		for (i = min_number ; i <= max_number ; i++) {
+			is_prime = 1;
+
+			j = 2;
+			while (j < i ){
+				is_prime *= (i%j != 0);
+				j++;
+			}
+
+			while (is_prime){
+				printf("%d ", i);
+				break;
+			}
 		}
 
-		while (is_prime){
-			printf("%d ", i);
-			break;
-		}
-	}
-	getch();
+		printf("do you want to continue? (Y/N) : ");
+		scanf(" %c",&end);
+	} while(end != 'N' && end != 'n');
+
 
 }
